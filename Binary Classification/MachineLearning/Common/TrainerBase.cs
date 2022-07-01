709fa7aaf6c0902d04b4cfed0a43e52b7c166241
Martin <marb61a@gmail.com>
2022-07-01 17:57:44 +0100

    Added TrainerBase class and updated notes.txt

--- Binary Classification/MachineLearning/Common/TrainerBase.cs
+++ Binary Classification/MachineLearning/Common/TrainerBase.cs
@@ -2,6 +2,21 @@ namespace BinaryClassification.MachineLearning.Common
 {
     public class TrainerBase<TParameters> : ITrainerBase
     {
+        public string Name => throw new NotImplementedException();
 
+        public BinaryClassificationMetrics Evaluate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Fit(string trainingFileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
