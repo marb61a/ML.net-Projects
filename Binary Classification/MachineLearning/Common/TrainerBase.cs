cb67aa1a9e363ee98b4f94c9ce746f7dd267e8e1
Martin <marb61a@gmail.com>
2022-07-03 18:36:52 +0100

    Updated TraineBase class to allow for data splitting, modelling and model saving

--- Binary Classification/MachineLearning/Common/TrainerBase.cs
+++ Binary Classification/MachineLearning/Common/TrainerBase.cs
@@ -1,17 +1,38 @@
 namespace BinaryClassification.MachineLearning.Common
 {
     public class TrainerBase<TParameters> : ITrainerBase
+        where TParameters : class
     {
-        public string Name => throw new NotImplementedException();
+        public string Name { get; protected set; }
+
+        // Defines where the created model will be saved to on the file system
+        protected static string ModelPath => Path.Combine(AppContext.BaseDirectory, "classification.mdl");
+        protected readonly MLContext mLContext;
+
+        // Loads data and splits into test and train data
+        protected DataOperationsCatalog.TrainTestData _dataSplit;
+
+        // Uses the ML.Trainers package which is set in the usings file 
+        protected ITrainerEstimator<BinaryPredictionTransformer<TParameters>, TParameters> _model;
+        protected ITransformer _trainedModel;
+
+        protected TrainerBase()
+        {
+            mLContext = new MLContext(11);
+        }
 
         public BinaryClassificationMetrics Evaluate()
         {
             throw new NotImplementedException();
         }
 
+        // Check if file exists
         public void Fit(string trainingFileName)
         {
-            throw new NotImplementedException();
+            if(!File.Exists(trainingFileName))
+            {
+                throw new FileNotFoundException($"File {trainingFileName} does not exist");
+            }
         }
 
         public void Save()
--- Binary Classification/Usings.cs
+++ Binary Classification/Usings.cs
@@ -3,3 +3,4 @@ global using Microsoft.ML.Data;
 global using System;
 global using System.Collections.Generic;
 global using System.Linq;
+global using Microsoft.ML.Trainers;
