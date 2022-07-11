6e532e2fb6d4f33af868e78fd8163adf60f6ed5d
Martin <marb61a@gmail.com>
2022-07-11 18:49:28 +0100

    Added trainer files

---
+++ DecisionTreeClassification/MachineLearning/Trainers/DecisionTreeTrainer.cs
@@ -0,0 +1,17 @@
+using DecisionTreeClassification.MachineLearning.Common;
+
+namespace DecisionTreesClassification.MachineLearning.Trainers;
+
+public class DecisionTreeTrainer: TrainerBase<CalibratedModelParametersBase<FastTreeBinaryModelParameters, PlattCalibrator>>
+{
+    public DecisionTreeTrainer(int numberOfLeaves, int numberOfTrees, double learningRate = 0.2): base()
+    {
+        Name = $"Decision Tree-{numberOfLeaves}-{numberOfTrees}-{learningRate}";
+        _model = MlContext.BinaryClassification.Trainers.FastTree(
+            numberOfLeaves: numberOfLeaves,
+            numberOfTrees: numberOfTrees,
+            learningRate: learningRate
+        );
+    }
+    
+}
---
+++ DecisionTreeClassification/MachineLearning/Trainers/GamTrainer.cs
@@ -0,0 +1,12 @@
+using DecisionTreeClassification.MachineLearning.Common;
+
+namespace DecisionTreeClassification.MachineLearning.Trainers;
+
+public class GamTrainer : TrainerBase<CalibratedModelParametersBase<GamBinaryModelParameters, PlattCalibrator>>
+{
+    public GamTrainer() : base()
+    {
+        Name = "GAM";
+        _model = MlContext.BinaryClassification.Trainers.Gam();
+    }
+}
