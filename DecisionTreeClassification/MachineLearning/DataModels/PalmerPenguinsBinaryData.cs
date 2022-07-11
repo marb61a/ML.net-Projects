6c2d21879af332ad9ae4ba437d0e74a6c46c30c6
Martin <marb61a@gmail.com>
2022-07-11 17:57:54 +0100

    Added data model files

---
+++ DecisionTreeClassification/MachineLearning/DataModels/PalmerPenguinsBinaryData.cs
@@ -0,0 +1,15 @@
+namespace DecisionTreeClassification.MachineLearning.DataModels;
+
+// Models Palmer Penguins Binary Data
+public class PalmerPenguinsBinaryData
+{
+    [LoadColumn(0)]
+    public bool Label { get; set; }
+
+    [LoadColumn(2)]
+    public float CulmenLength { get; set; }
+
+    [LoadColumn(3)]
+    public float CulmenDepth { get; set; }
+
+}
---
+++ DecisionTreeClassification/MachineLearning/DataModels/PalmerPenguinsBinaryPrediction.cs
@@ -0,0 +1,7 @@
+namespace DecisionTreeClassification.MachineLearning.DataModels;
+
+// Models Palmer Penguins Binary Prediction
+public class PalmerPenguinsBinaryPrediction
+{
+    public string PredictedLabel { get; set; }
+}
