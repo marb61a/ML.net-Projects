cbc5326e04ed3a8e306629ad73138c659a336851
Martin <marb61a@gmail.com>
2022-07-14 17:13:13 +0100

    Added fit, save and evaluate functions to trainerbase

--- DecisionTreesRegression/MachineLearning/Common/TrainerBase.cs
+++ DecisionTreesRegression/MachineLearning/Common/TrainerBase.cs
@@ -11,5 +11,37 @@ namespace DecisionTreesRegression.MachineLearning.Common
         protected DataOperationsCatalog.TrainTestData _dataSplit;
         protected ITrainerEstimator<RegressionPredictionTransformer<TParameters>, TParameters> _model;
         protected ITransformer _trainedModel;
+
+        protected TrainerBase()
+        {
+            mlContext = new MLContext(11);
+        }
+
+        public void Fit(string trainingFileName)
+        {
+            if(!File.Exists(trainingFileName))
+            {
+                throw new FileNotFoundException($"File {trainingFileName} doesn't exist.");
+            }
+
+            _dataSplit = LoadAndPrepareData(trainingFileName);
+            var dataProcessPipeline = BuildDataProcessingPipeline();
+            var trainingPipeline = dataProcessPipeline.Append(_model);
+            _trainedModel = trainingPipeline.Fit(_dataSplit.TrainSet);
+
+        }
+
+        // Evaluate the trained model
+        public RegressionMetrics Evaluate()
+        {
+            var testSetTransform = _trainedModel.Transform(_dataSplit.TestSet);
+            return mlContext.Regression.Evaluate(testSetTransform);
+        }
+
+        // Save the model in a file
+        public void Save()
+        {
+            mlContext.Model.Save(_trainedModel, _dataSplit.TrainSet.Schema, ModelPath);
+        }
     }
 }
