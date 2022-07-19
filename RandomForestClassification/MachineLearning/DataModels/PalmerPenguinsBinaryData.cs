6e27c623903d8ac42c7ef495dd53737c81c7c01c
Martin <marb61a@gmail.com>
2022-07-19 14:12:01 +0100

    Added data models files and global usings file

---
+++ RandomForestClassification/MachineLearning/DataModels/PalmerPenguinsBinaryData.cs
@@ -0,0 +1,15 @@
+namespace RandomForestClassification.MachineLearning.DataModels;
+
+// Models Palmer Penguins Binary Data
+public class PalmerPenguinsBinaryData
+{
+    [LoadColumn(0)]
+    public bool Label { get; set; }
+
+    [LoadColumn(2)]
+    public float CulmenLength { get; set; }
+
+    [LoadColumn(3)]
+    public float CulmenDepth { get; set; }
+
+}
---
+++ RandomForestClassification/MachineLearning/DataModels/PalmerPenguinsBinaryPrediction.cs
@@ -0,0 +1,7 @@
+namespace RandomForestClassification.MachineLearning.DataModels;
+
+// Models Palmer Penguins Binary Prediction
+public class PalmerPenguinsBinaryPrediction
+{
+    public bool PredictedLabel { get; set; }
+}
