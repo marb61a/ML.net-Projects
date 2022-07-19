23fbc0f8ce39ab66470f988300d7c0dd0d3417cf
Martin <marb61a@gmail.com>
2022-07-19 21:36:20 +0100

    Added random forest trainer file

---
+++ RandomForestClassification/MachineLearning/Trainers/RandomForestTrainer.cs
@@ -0,0 +1,16 @@
+using RandomForestClassification.MachineLearning.Common;
+
+namespace RandomForestClassification.MachineLearning.Trainers
+{
+    public class RandomForestTrainer : TrainerBase<FastForestBinaryModelParameters>
+    {
+        public RandomForestTrainer(int numberOfLeaves, int numberOfTrees) : base()
+        {
+            Name = $"Random Forest: {numberOfLeaves}-{numberOfTrees}";
+            _model = mlContext.BinaryClassification.Trainers.FastForest(
+                                            numberOfLeaves: numberOfLeaves,
+                                        numberOfTrees: numberOfTrees);
+        }
+    }
+
+}
