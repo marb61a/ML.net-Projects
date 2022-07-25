e73f67ef7361ac2622df5770e4a37c9e8fb7dbfc
Martin <marb61a@gmail.com>
2022-07-25 14:24:38 +0100

    Updated TrainerBase common file with implementation of interface defined methods

--- SVM/MachineLearning/Common/TrainerBase.cs
+++ SVM/MachineLearning/Common/TrainerBase.cs
@@ -18,9 +18,48 @@ namespace SVM.MachineLearning.Common
             mlContext = new MLContext(11);
         }
 
-        public void Fit()
+        public void Fit(string trainingFileName)
         {
+            if(!File.Exists(trainingFileName))
+            {
+                throw new FileNotFoundException($"File {trainingFileName} does not exist");
+            }
 
+            _dataSplit = LoadAndPrepareData(trainingFileName);
+            var dataProcessPipeline = BuildDataProcessingPipeline();
+            var trainingPipeline = dataProcessPipeline.Append(_model);
+            _trainedModel = trainingPipeline.Fit(_dataSplit.TrainSet);
         }
+
+        public BinaryClassificationMetrics Evaluate()
+        {
+            var testSetTransform = _trainedModel.Transform(_dataSplit.TestSet);
+            return mlContext.BinaryClassification.EvaluateNonCalibrated(testSetTransform);
+        }
+
+        public void Save()
+        {
+            mlContext.Model.Save(_trainedModel, _dataSplit.TrainSet.Schema, ModelPath);
+        }
+
+        private EstimatorChain<NormalizingTransformer>BuildDataProcessingPipeline()
+        {
+            var dataProcessPipeline = mlContext.Transforms.Concatenate("Features",
+                                            nameof(PalmerPenguinsBinaryData.CulmenDepth),
+                                            nameof(PalmerPenguinsBinaryData.CulmenLength)
+                                        )
+            .Append(mlContext.Transforms.NormalizeMinMax("Features", "Features"))
+            .AppendCacheCheckpoint(mlContext);
+
+            return dataProcessPipeline;
+        }
+
+        private DataOperationsCatalog.TrainTestData LoadAndPrepareData(string trainingFileName)
+        {
+            var trainingDataView = mlContext.Data.LoadFromTextFile<PalmerPenguinsBinaryData>
+                                        (trainingFileName, hasHeader: true, separatorChar: ',');
+            return mlContext.Data.TrainTestSplit(trainingDataView, testFraction: 0.3);
+        }
+
     }
 }
