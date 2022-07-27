d6336f0848f8fccf3951c972008290a8006552cf
Martin <marb61a@gmail.com>
2022-07-27 14:32:52 +0100

    Updated TrainerBase file

--- Clustering/MachineLearning/Common/TrainerBase.cs
+++ Clustering/MachineLearning/Common/TrainerBase.cs
@@ -31,5 +31,45 @@ namespace Clustering.MachineLearning.Common
             var trainingPipeline = dataProcessPipeline.Append(_model);
             _trainedModel = trainingPipeline.Fit(_dataSplit.TrainSet);
         }
+
+        public ClusteringMetrics Evaluate()
+        {
+            var testSetTransform = _trainedModel.Transform(_dataSplit.TestSet);
+            return mlContext.Clustering.Evaluate(
+                data: testSetTransform,
+                labelColumnName: "PredictedLabel",
+                scoreColumnName: "Score",
+                featureColumnName: "Features"
+            );
+        }
+
+        public void Save()
+        {
+            mlContext.Model.Save(_trainedModel, _dataSplit.TrainSet.Schema, ModelPath);
+        }
+
+        private EstimatorChain<ColumnConcatenatingTransformer>BuildDataProcessingPipeline()
+        {
+            var dataProcessPipeline = mlContext.Transforms.Text.FeaturizeText(inputColumnName: "Sex", outputColumnName: "SexFeaturized")
+                .Append(MlContext.Transforms.Text.FeaturizeText(inputColumnName: "Island", outputColumnName: "IslandFeaturized"))
+                .Append(MlContext.Transforms.Concatenate(
+                    "Features",
+                    "IslandFeaturized",
+                    nameof(PalmerPenguinsData.CulmenLength),
+                    nameof(PalmerPenguinsData.CulmenDepth),
+                    nameof(PalmerPenguinsData.BodyMass),
+                    nameof(PalmerPenguinsData.FliperLength),
+                    "SexFeaturized"
+                ))
+                .AppendCacheCheckpoint(mlContext);
+
+            return dataProcessPipeline;
+        }
+
+        private DataOperationsCatalog.TrainTestData LoadAndPrepareData(string trainingFileName)
+        {
+            var trainingDataView = mlContext.Data.LoadFromTextFile<PalmerPenguinsData>(trainingFileName, hasHeader: true, separatorChar: ',');
+            return mlContext.Data.TrainTestSplit(trainingDataView, testFraction: 0.3);
+        }
     }
 }
