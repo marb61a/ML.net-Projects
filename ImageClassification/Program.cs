5489e6b16ca3129fc89210e4b309e6c3c4c90ab8
Martin <marb61a@gmail.com>
2022-08-03 21:18:45 +0100

    Updated program.cs to run predictions

--- ImageClassification/Program.cs
+++ ImageClassification/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ImageClassification.MachineLearning;
+
+// Add path to data file to be loaded
+var dataLoader = new DataLoader("");
+
+var trainer = new ImageClassifier(ImageClassificationTrainer.Architecture.MobilenetV2, dataLoader);
+trainer.Fit();
+trainer.Save();
+
+var predictor = new Predictor(dataLoader);
+
+Console.WriteLine("Make predictions on test dataset:");
+var predictions = predictor.MakeTestDatasetPredictions();
+
+foreach (var prediction in predictions)
+{
+    string imageName = Path.GetFileName(prediction.ImagePath);
+    Console.WriteLine($"Image: {imageName} | Actual Value: {prediction.Label} | Predicted Value: {prediction.PredictedLabel}");
+}
