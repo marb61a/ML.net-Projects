23008628670796957299ff4cf9a16acec3a0cfa3
Martin <marb61a@gmail.com>
2022-08-03 13:56:18 +0100

    Updated data loader methods

--- ImageClassification/MachineLearning/DataLoader.cs
+++ ImageClassification/MachineLearning/DataLoader.cs
@@ -38,11 +38,39 @@ namespace ImageClassification.MachineLearning
         private void LoadAndPrepareData(EstimatorChain<ImageLoadingTransformer> dataProcessPipeline)
         {
             IEnumerable<ImageData> images = LoadImages(_trainingFolder);
+            var imageData = _mlContext.Data.LoadFromEnumerable(images);
+            var shuffledImageData = _mlContext.Data.ShuffleRows(imageData);
+
+            var preparedData = dataProcessPipeline
+                .Fit(shuffledImageData)
+                .Transform(shuffledImageData);
+            
+            var trainSplit = _mlContext.Data.TrainTestSplit(data: preparedData, testFraction: 0.3);
+            var validationTestSplit = _mlContext.Data.TrainTestSplit(trainSplit.TestSet);
+
+            TrainSet = trainSplit.TrainSet;
+            ValidationSet = validationTestSplit.TrainSet;
+            TestSet = validationTestSplit.TestSet;
         }
 
         private IEnumerable<ImageData> LoadImages(string trainingFolder)
         {
-            
+            var files = Directory.GetFiles(trainingFolder, "*", searchOption: SearchOption.AllDirectories);
+
+            foreach(var file in files)
+            {
+                if((Path.GetExtension(file) != ".jpg") && (Path.GetExtension(file) != ".png"))
+                    continue;
+                
+                var label = Path.GetFileName(file);
+                label = label.Substring(0, 3);
+
+                yield return new ImageData()
+                {
+                    ImagePath = file,
+                    Label = label
+                };
+            }
         }
     }
 }
