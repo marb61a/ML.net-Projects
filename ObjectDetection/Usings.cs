c853d8529095d5024e79602d165edb8cf78a2db4
Martin <marb61a@gmail.com>
2022-08-05 14:46:56 +0100

    Added usings file and dependencies

---
+++ ObjectDetection/Usings.cs
@@ -0,0 +1,9 @@
+global using Microsoft.ML;
+global using Microsoft.ML.Data;
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using Microsoft.ML.Trainers;
+global using Microsoft.ML.Transforms.Image;
+global using Microsoft.ML.Calibrators;
+global using Microsoft.ML.Vision;
