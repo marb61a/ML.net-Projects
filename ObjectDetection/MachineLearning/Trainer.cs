f8a2e8516590919664feb60b5921df7e342a7ed3
Martin <marb61a@gmail.com>
2022-08-08 21:20:42 +0100

    Updated trainer file

--- ObjectDetection/MachineLearning/Trainer.cs
+++ ObjectDetection/MachineLearning/Trainer.cs
@@ -20,7 +20,35 @@ namespace ObjectDetection.MachineLearning
                 imageWidth: 416,
                 imageHeight: 416,
                 resizing: ResizingKind.IsoPad
-            );
+            )
+            .Append(_mlContext.Transforms.ExtractPixels(
+                outputColumnName: "input_1:0",
+                scaleImage: 1f / 255f,
+                interleavePixelColors: true
+            ))
+            .Append(_mlContext.Transforms.ApplyOnnxModel(
+                outputColumnNames: new[]
+                {
+                            "Identity:0",
+                            "Identity_1:0",
+                            "Identity_2:0"
+                },
+                inputColumnNames: new[]
+                {
+                            "input_1:0"
+                },
+                shapeDictionary: new Dictionary<string, int[]>()
+                {
+                            { "input_1:0", new[] { 1, 416, 416, 3 } },
+                            { "Identity:0", new[] { 1, 52, 52, 3, 85 } },
+                            { "Identity_1:0", new[] { 1, 26, 26, 3, 85 } },
+                            { "Identity_2:0", new[] { 1, 13, 13, 3, 85 } },
+                },
+                modelFile: yoloModelPath,
+                gpuDeviceId: null,
+                fallbackToCpu: true));
+
+            return pipeline.Fit(_mlContext.Data.LoadFromEnumerable(new List<ImageData>()));;
         }
     }
 }
