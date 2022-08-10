388689b73f1bb6604772e9404e0f854056e123c6
Martin <marb61a@gmail.com>
2022-08-10 21:29:21 +0100

    Added TrainerBase file

---
+++ SentimentAnalysis/MachineLearning/Common/TrainerBase.cs
@@ -0,0 +1,38 @@
+using SentimentAnalysis.MachineLearning.DataModels;
+
+namespace SentimentAnalysis.MachineLearning.Common
+{
+    // The base class for trainers
+    public abstract class TrainerBase<TParameters>: ITrainerBase where TParameters: class
+    {
+        public string Name { get; protected set; }
+
+        protected static string ModelPath => Path.Combine(AppContext.BaseDirectory, "classification.mdl");
+
+        protected readonly MLContext mlContext;
+
+        protected DataOperationsCatalog.TrainTestData _dataSplit;
+        protected ITrainerEstimator<BinaryPredictionTransformer<TParameters>, TParameters> _model;
+        protected ITransformer _trainedModel;
+
+        protected TrainerBase()
+        {
+            mlContext = new MLContext(11);
+        }
+
+        // Trains model on defined data
+        public void Fit(string trainingFileName)
+        {
+            if (!File.Exists(trainingFileName))
+            {
+                throw new FileNotFoundException($"File {trainingFileName} doesn't exist.");
+            }
+
+            _dataSplit = LoadAndPrepareData(trainingFileName);
+            var dataProcessPipeline = BuildDataProcessingPipeline();
+            var trainingPipeline = dataProcessPipeline.Append(_model);
+
+            _trainedModel = trainingPipeline.Fit(_dataSplit.TrainSet);
+        }
+    }
+}
