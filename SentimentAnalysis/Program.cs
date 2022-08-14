be607a974c11c648e9b9e3ea67a18b6ccd7b6bd6
Martin <marb61a@gmail.com>
2022-08-14 14:52:13 +0100

    Updated program.cs to run predictions

--- SentimentAnalysis/Program.cs
+++ SentimentAnalysis/Program.cs
@@ -26,3 +26,35 @@ var trainers = new List<ITrainerBase>
     new RandomForestTrainer(10, 20)
 };
 
+trainers.ForEach(t => TrainEvaluatePredict(t, newSample));
+
+void TrainEvaluatePredict(ITrainerBase trainer, SentimentData newSample)
+{
+    Console.WriteLine("*******************************");
+    Console.WriteLine($"{ trainer.Name }");
+    Console.WriteLine("*******************************");
+
+    string fileName = "imdb_labelled.txt";
+    string path = Path.Combine(Environment.CurrentDirectory, @"Data\", fileName);
+
+    trainer.Fit(path.ToString());
+
+    var modelMetrics = trainer.Evaluate();
+
+    Console.WriteLine($"Accuracy: {modelMetrics.Accuracy:0.##}{Environment.NewLine}" +
+                      $"F1 Score: {modelMetrics.F1Score:#.##}{Environment.NewLine}" +
+                      $"Positive Precision: {modelMetrics.PositivePrecision:#.##}{Environment.NewLine}" +
+                      $"Negative Precision: {modelMetrics.NegativePrecision:0.##}{Environment.NewLine}" +
+                      $"Positive Recall: {modelMetrics.PositiveRecall:#.##}{Environment.NewLine}" +
+                      $"Negative Recall: {modelMetrics.NegativeRecall:#.##}{Environment.NewLine}" +
+                      $"Area Under Precision Recall Curve: {modelMetrics.AreaUnderPrecisionRecallCurve:#.##}{Environment.NewLine}");
+
+    trainer.Save();
+
+    var predictor = new Predictor();
+    var prediction = predictor.Predict(newSample);
+    Console.WriteLine("------------------------------");
+    Console.WriteLine($"Prediction: {prediction.Prediction:#.##}");
+    Console.WriteLine($"Probability: {prediction.Probability:#.##}");
+    Console.WriteLine("------------------------------");
+}
