05190b0f58a85d0c5a7c0cc7a666a72e14502b53
Martin <marb61a@gmail.com>
2022-08-16 13:56:05 +0100

    Added usings file and BertInput data model file

---
+++ NLPBERT/MachineLearning/DataModel/BertInput.cs
@@ -0,0 +1,22 @@
+namespace NLPBERT.MachineLearning.DataModel
+{
+    public class BertIuput
+    {
+        [VectorType(1, 256)]
+        [ColumnName("input_ids:0")]
+        public long[] InputId { get; set; }
+
+        [VectorType(1, 256)]
+        [ColumnName("input_mask:0")]
+        public long[] InputMask { get; set; }
+
+        [VectorType(1, 256)]
+        [ColumnName("segment_ids:0")]
+        public long[] SegmentIds { get; set; }
+
+        [VectorType(1)]
+        [ColumnName("unique_ids_raw_output___9:0")]
+        public long[] UniqueIds { get; set; }
+        
+    }
+}
