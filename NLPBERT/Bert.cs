28892b79c0addd0f196ce1ba6a7c4688b1eaa8b4
Martin <marb61a@gmail.com>
2022-08-17 13:56:29 +0100

    Updated bert file with predict method

--- NLPBERT/Bert.cs
+++ NLPBERT/Bert.cs
@@ -4,8 +4,44 @@ using NLPBERT.MachineLearning.DataModel;
 
 namespace NLPBERT
 {
-    public class NLPBERT
+    public class Bert
     {
-        
+        private readonly BertUncasedBaseTokenizer _tokeniser;
+        private Predictor _predictor;
+
+        public Bert(string bertModelPath)
+        {
+            _tokeniser = new BertUncasedBaseTokenizer();
+            var trainer = new Trainer();
+
+            var trainedModel = trainer.BuildAndTrain(bertModelPath);
+            _predictor = new Predictor(trainedModel);
+        }
+
+        public (List<string> tokens, float probablity) Predict(string context, string question)
+        {
+            var tokens = _tokeniser.Encode(256, question, context);
+            var input = new BertInput()
+            {
+                InputId = tokens.Select(t => t.InputIds).ToArray(),
+                SegmentIds = tokens.Select(t => t.TokenTypeIds).ToArray(),
+                InputMask = tokens.Select(t => t.AttentionMask).ToArray(),
+                UniqueIds = new long[] { 0 }
+            };
+
+            var predictions = _predictor.Predict(input);
+            var contextStart = tokens.FindIndex(o => o.InputIds == 102);
+
+            var (startIndex, endIndex, probability) = GetBestPrediction(predictions, contextStart, 20, 30);
+
+            var predictedTokens = input.InputId
+                .Skip(startIndex)
+                .Take(endIndex + 1 - startIndex)
+                .Select(o => _tokeniser.IdToToken((int)o))
+                .ToList();
+
+            var connectedTokens = _tokeniser.Untokenize(predictedTokens);
+            return (connectedTokens, probability);
+        }
     }
 }
